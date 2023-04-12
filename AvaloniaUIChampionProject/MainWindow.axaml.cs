0711e2ba830899dc19ff47fb1a2dd1a5e8028933
Net2Fox <trakounet.fox@gmail.com>
2023-04-12 17:59:56 +0300

    Создал проект

---
+++ AvaloniaUIChampionProject/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUIChampionProject;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
